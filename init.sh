fbcedf1ac259f6b37410491cd8774dc844daa2d6
myguddy <seungil.han@gmail.com>
2019-07-05 13:39:51 +0900

    fix

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+SECRET_DIR="./secret"
+
+if [ ! -d $DATADIR ]; then
+  mkdir -p $DATADIR;
+fi
 
 echo "Creating default wallet's password"
-cleos wallet create -f ./secret/pin
-EOSIO_PASSWORD=`cat ./secret/pin`
+cleos wallet create -f $SECRET_DIR/pin
+EOSIO_PASSWORD=`cat $SECRET_DIR/pin`
 
 echo "password is $EOSIO_PASSWORD"
 
