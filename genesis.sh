ca1c6681386dc56c2938acfe9b2e9755b06505fa
myguddy <seungil.han@gmail.com>
2019-07-05 16:05:36 +0900

    ..

--- genesis.sh
+++ genesis.sh
@@ -11,6 +11,19 @@ function genKey(){
   cleos wallet import --private-key $ACC_PRIVATE_KEY
 }
 
+ROLE="keosd"
+LOG_DIR="./log"
+
+if [ ! -d $LOG_DIR ]; then
+  mkdir -p $LOG_DIR;
+fi
+
+keosd --http-server-address 0.0.0.0:9900 \
+--http-validate-host false \
+>> $LOG_DIR"/$ROLE.log" 2>&1 & \
+echo $! > $DATADIR"/$ROLE.pid"
+
+
 
 SECRET_DIR="./secret"
 
